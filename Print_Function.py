a67ee18f295a83b9b7ff9dd292509d934f9e57d9
Khaitlin Bernaldez <92839451+khaitmb@users.noreply.github.com>
2022-05-31 15:38:08 -0700

    Update Print_Function.py

--- Print_Function.py
+++ Print_Function.py
@@ -1,5 +1,5 @@
 # Without using any string methods, try to print the following: 123...n
 # Print the list of intergers from 1 through n as a string, without spaces.
 
-# HackRank Accepted Solution:
+# HACKERRANK ACCEPTED SOLUTION
 print(*range(1, n+1), sep ='')
