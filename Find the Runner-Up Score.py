8184eeaf3f06f160d86a8a3970badad51b216b4d
Khaitlin Bernaldez <92839451+khaitmb@users.noreply.github.com>
2022-05-31 15:37:46 -0700

    Create Find the Runner-Up Score.py

---
+++ Find the Runner-Up Score.py
@@ -0,0 +1,16 @@
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. 
+# Store them in a list and find the score of the runner-up.
+
+# INPUT FORMAT
+# The first line contains n. The second line contains an array A[] of n integers each separated by a space.
+
+# OUTPUT FORMAT
+# Print the runner-up score.
+
+# HACKER-RANK ACCEPTED SOLUTION
+score = []
+for i in arr:
+    if i not in score:
+        score.append(i)
+score.sort(reverse = True)
+print(score[1])
