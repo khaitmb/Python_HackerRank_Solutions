f73e6f2e140fbe6351110502374110d810945c48
Khaitlin Bernaldez <92839451+khaitmb@users.noreply.github.com>
2022-06-01 10:57:44 -0700

    Update Find the Runner-Up Score.py

--- Find the Runner-Up Score.py
+++ Find the Runner-Up Score.py
@@ -1,3 +1,5 @@
+# Skills Used: input, for loop, if-else statement, sort, lists
+
 # Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. 
 # Store them in a list and find the score of the runner-up.
 
