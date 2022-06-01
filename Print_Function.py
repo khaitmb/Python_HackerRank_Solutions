b6ccd606148a95d463289fcbe22e59aafe7745b8
Khaitlin Bernaldez <92839451+khaitmb@users.noreply.github.com>
2022-06-01 10:58:07 -0700

    Update Print_Function.py

--- Print_Function.py
+++ Print_Function.py
@@ -1,3 +1,5 @@
+# Skills Used: modified print function
+
 # Without using any string methods, try to print the following: 123...n
 # Print the list of intergers from 1 through n as a string, without spaces.
 
